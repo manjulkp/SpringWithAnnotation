f3b83a198a1d489239b05311c99ac9a48a10f957
Manjula K Pshetty <manjulakp29@gmail.com>
2020-08-17 07:55:11 +0100

    Update Person.java

--- src/com/uttara/spring/Person.java
+++ src/com/uttara/spring/Person.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("person")
 public class Person {
 	
 	@Value("manjula")
